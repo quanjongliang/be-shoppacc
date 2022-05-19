7d10455d0918c142b4fd3441107e71e47176f375
Quill Liang <lhongquan.1998@gmail.com>
2022-05-19 08:50:46 +0700

    update orm

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const config = new DocumentBuilder()
-    .setTitle('Tempest Genshin ')
-    .setDescription('Tempest Genshin API description')
-    .setVersion('1.0')
-    .addTag('tempest-genshin')
+    .setTitle("Tempest Genshin ")
+    .setDescription("Tempest Genshin API description")
+    .setVersion("1.0")
+    .addTag("tempest-genshin")
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
   await app.listen(process.env.PORT || 4444);
 }
 bootstrap();
