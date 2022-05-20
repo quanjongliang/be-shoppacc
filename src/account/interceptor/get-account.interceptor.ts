3a505f6b3f322fee323f066f9dd971668bc10ddc
Quill Liang <lhongquan.1998@gmail.com>
2022-05-20 09:00:13 +0700

    interceptor account

--- src/account/interceptor/get-account.interceptor.ts
+++ src/account/interceptor/get-account.interceptor.ts
@@ -1,30 +1,42 @@
-import { BaseQueryResponse } from '@/core';
-import { Account } from '@/entity';
+import { BaseQueryResponse } from "@/core";
+import { Account, TAG_TYPE } from "@/entity";
 import {
   Injectable,
   NestInterceptor,
   ExecutionContext,
   CallHandler,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
 @Injectable()
 export class GetAccountInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+    console.log("Before...");
     return next.handle().pipe(
       map((data: BaseQueryResponse<Account>) => {
         const formattedData = data.data.map((d) => ({
           ...d,
           cloundinary: d.cloundinary.map((cl) => cl.url || cl.secure_url),
           user: d.user.username,
+          character: d.character
+            ? d.character
+            : d.tags
+                .filter(({ type }) => type === TAG_TYPE.CHARACTER)
+                .map(({ slug }) => slug)
+                .join(","),
+          weapon: d.weapon
+            ? d.weapon
+            : d.tags
+                .filter(({ type }) => type === TAG_TYPE.WEAPON)
+                .map(({ slug }) => slug)
+                .join(","),
         }));
         return {
           ...data,
           data: formattedData,
         };
-      }),
+      })
     );
   }
 }
