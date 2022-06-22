41d49ecb8b58214ddb6c66af97b78b52f874e2d8
Quill Liang <larry@keyreply.com>
2022-06-22 19:23:12 +0700

    add ssl mode true

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,12 +10,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
       // username: process.env["POSTGRES_USER"] || "postgres",
       // password: process.env["POSTGRES_PASSWORD"] || "postgres",
       // database: process.env["POSTGRES_DB"] || "acc",
-      host: process.env["POSTGRES_HOST"] || "db-postgresql-sgp1-26012-do-user-11469090-0.b.db.ondigitalocean.com",
+      host:
+        process.env["POSTGRES_HOST"] ||
+        "db-postgresql-sgp1-26012-do-user-11469090-0.b.db.ondigitalocean.com",
       port: parseInt(process.env["POSTGRES_PORT"] || "25060", 10),
       username: process.env["POSTGRES_USER"] || "doadmin",
       password: process.env["POSTGRES_PASSWORD"] || "AVNS_iJkMaPG3cQTcceBSgsI",
       database: process.env["POSTGRES_DB"] || "defaultdb",
-      ssl:{require:true},
+      ssl: true,
       // url: process.env.DATABASE_URL,
       // ssl: { rejectUnauthorized: false },
       synchronize: !!JSON.parse(process.env["TYPEORM_SYNCHRONIZE"] || "true"),
