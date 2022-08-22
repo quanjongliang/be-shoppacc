6bc031259d00f696ee37efb67b4a4a67b81e35ef
Quill Liang <larry@keyreply.com>
2022-08-22 18:24:45 +0700

    as

--- src/account/service/account.service.ts
+++ src/account/service/account.service.ts
@@ -147,8 +147,7 @@ export class AccountService {
       .andWhere("account.game =:game", { game })
       .take(limit)
       .skip(offset)
-      .andWhere(`account.code ILIKE '%${queryString}%'`)
-      .addOrderBy("account.soldAt", isSoldValue ? "DESC" : "ASC");
+      .andWhere(`account.code ILIKE '%${queryString}%'`);
     weaponList.forEach((w) => {
       findAccountQuery.andWhere(`account.weapon ILIKE '%${w}%'`);
     });
